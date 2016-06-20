61662d7ab6a77f2412bdd13794009f96e4337dd1
Camiloasc1 <camiloasc1@gmail.com>
2016-06-19 20:42:05 -0500

    Basic project schema.

---
+++ Assets/Procedural2DTileWorld/Scripts/Chunk.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Procedural2DTileWorld
+{
+    public class Chunk : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
---
+++ Assets/Procedural2DTileWorld/Scripts/WorldBuilder.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+namespace Procedural2DTileWorld
+{
+    public class WorldBuilder : MonoBehaviour
+    {
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+}
