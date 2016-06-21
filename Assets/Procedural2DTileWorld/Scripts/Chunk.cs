334df160432a7b7ba986f23a85f84f7441170b53
Camiloasc1 <camiloasc1@gmail.com>
2016-06-21 15:33:55 -0500

    Basic chunk and world.

--- Assets/Procedural2DTileWorld/Scripts/Chunk.cs
+++ Assets/Procedural2DTileWorld/Scripts/Chunk.cs
@@ -5,6 +5,41 @@ namespace Procedural2DTileWorld
 {
     public class Chunk : MonoBehaviour
     {
+        //[Header("Health Settings")]
+        [Tooltip("Wich tiled world this chunk belongs")] [SerializeField] private World world;
+        [Tooltip("(X,Y) Position in tiled world.")] [SerializeField] private Vector2 position;
+        [Tooltip("Size of the chunk.")] [SerializeField] private uint size;
+
+        private GameObject[,] tiles;
+
+        public World World
+        {
+            get { return world; }
+            set { world = value; }
+        }
+
+        public Vector2 Position
+        {
+            get { return position; }
+            set { position = value; }
+        }
+
+        public uint Size
+        {
+            get { return size; }
+            set
+            {
+                size = value;
+                Generate();
+            }
+        }
+
+        // Awake is called when the script instance is being loaded
+        void Awake()
+        {
+            Generate();
+        }
+
         // Use this for initialization
         void Start()
         {
@@ -13,6 +48,24 @@ namespace Procedural2DTileWorld
         // Update is called once per frame
         void Update()
         {
+        }
+
+        /// <summary>
+        /// Generate the tiles in this chunk.
+        /// </summary>
+        public void Generate()
+        {
+            tiles = new GameObject[size, size];
+            for (int i = 0; i < tiles.GetLength(0); i++)
+            {
+                for (int j = 0; j < tiles.GetLength(1); j++)
+                {
+                    //TODO Use object pool
+                    tiles[i, j] = Instantiate(world.Terrain.Cobblestone);
+                    tiles[i, j].transform.parent = transform;
+                    tiles[i, j].transform.localPosition = Vector3.right*i + Vector3.down*j;
+                }
+            }
         }
     }
 }
---
+++ Assets/Procedural2DTileWorld/Scripts/World.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+
+namespace Procedural2DTileWorld
+{
+    public class World : MonoBehaviour
+    {
+        [Tooltip("Size of the chunks.")] [SerializeField] private uint chunkSize = 16;
+        [Tooltip("Prefab of chunk.")] [SerializeField] private GameObject chunk;
+        [Tooltip("Enviroment tiles prefabs.")] [SerializeField] private Enviroment enviroment;
+        [Tooltip("Terrain tiles prefabs.")] [SerializeField] private Terrain terrain;
+        [Tooltip("Settings for procedural generation.")] [SerializeField] private Settings settings;
+
+        private Chunk[,] chunks;
+
+        public Enviroment Enviroment
+        {
+            get { return enviroment; }
+        }
+
+        public Terrain Terrain
+        {
+            get { return terrain; }
+        }
+
+        public Settings Settings
+        {
+            get { return settings; }
+        }
+
+        // Awake is called when the script instance is being loaded
+        void Awake()
+        {
+            chunks = new Chunk[1, 1];
+            for (int i = 0; i < chunks.GetLength(0); i++)
+            {
+                for (int j = 0; j < chunks.GetLength(1); j++)
+                {
+                    //TODO Use object pool
+                    GameObject chunkGameObject = Instantiate(chunk);
+                    chunks[i, j] = chunkGameObject.GetComponent<Chunk>();
+                    chunks[i, j].transform.parent = transform;
+                    chunks[i, j].transform.localPosition = Vector3.right*i + Vector3.down*j;
+                    chunks[i, j].World = this;
+                    chunks[i, j].Size = chunkSize;
+                }
+            }
+        }
+
+        // Use this for initialization
+        void Start()
+        {
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        }
+    }
+
+    [Serializable]
+    public struct Enviroment
+    {
+        [SerializeField] public GameObject DeadBush;
+        [SerializeField] public GameObject Flower;
+        [SerializeField] public GameObject Mushroom;
+        [SerializeField] public GameObject TallGrass;
+        [SerializeField] public GameObject Tree;
+        [SerializeField] public GameObject WaterLily;
+    }
+
+    [Serializable]
+    public struct Terrain
+    {
+        [SerializeField] public GameObject Cobblestone;
+        [SerializeField] public GameObject Dirt;
+        [SerializeField] public GameObject Grass;
+        [SerializeField] public GameObject Gravel;
+        [SerializeField] public GameObject Podzol;
+        [SerializeField] public GameObject Sand;
+        [SerializeField] public GameObject Sandstone;
+        [SerializeField] public GameObject Water;
+    }
+
+    [Serializable]
+    public struct Settings
+    {
+        [SerializeField] public float Water;
+        [SerializeField] public float Sand;
+        [SerializeField] public float Grass;
+        [SerializeField] public float Tree;
+    }
+}
