300bd2fff925b93fc24b2d45d5da3510ab18bbff
Camiloasc1 <camiloasc1@gmail.com>
2016-07-11 20:48:56 -0500

    New pooling system.
    Work in progress.

---
+++ Assets/PoolManager/Scripts/ObjectPool.cs
@@ -0,0 +1,114 @@
+﻿using System;
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PoolingSystem
+{
+    public class ObjectPool : MonoBehaviour, IObjectPool
+    {
+        [SerializeField] [Tooltip("The base prefab of this pool")] private GameObject _prefab;
+        [SerializeField] [Tooltip("The minimum number of objects to keep")] private uint _min;
+        [SerializeField] [Tooltip("The maximum number of objects to keep")] private uint _max;
+        [SerializeField] [Range(0.0f, 1.0f)] [Tooltip("The target usage ratio")] private float _usageRatio;
+        private readonly HashSet<GameObject> _active = new HashSet<GameObject>();
+        private readonly Stack<GameObject> _inactive = new Stack<GameObject>();
+
+        public GameObject Prefab
+        {
+            get { return _prefab; }
+        }
+
+        public bool IsPredefined { get; private set; }
+
+        public void FromPredefined(PredefinedObjectPool value)
+        {
+            IsPredefined = true;
+            _prefab = value.Prefab;
+            _min = value.Min;
+            _max = value.Max;
+            _usageRatio = value.UsageRatio;
+        }
+        
+        public GameObject Spawn()
+        {
+            if (_inactive.Count == 0)
+                return null;
+
+            var element = _inactive.Pop();
+            _active.Add(element);
+            element.transform.parent = null;
+            element.SendMessage("OnSpawn");
+            element.SetActive(true);
+            return element;
+        }
+
+        public GameObject Spawn(Transform transform)
+        {
+            return Spawn(transform, null);
+        }
+
+        public GameObject Spawn(Transform transform, Transform parent)
+        {
+            return Spawn(transform.position, transform.rotation, parent);
+        }
+
+        public GameObject Spawn(Vector3 position, Quaternion rotation)
+        {
+            return Spawn(position, rotation, null);
+        }
+
+        public GameObject Spawn(Vector3 position, Quaternion rotation, Transform parent)
+        {
+            if (_inactive.Count == 0)
+                return null;
+
+            var element = Spawn();
+            element.transform.position = position;
+            element.transform.rotation = rotation;
+            element.transform.parent = parent;
+            return element;
+        }
+
+        public void Despawn()
+        {
+            foreach (var instance in _active)
+            {
+                Despawn(instance);
+                break;
+            }
+        }
+
+        public void Despawn(GameObject element)
+        {
+            if (!_active.Contains(element))
+                return;
+
+            _active.Remove(element);
+            _inactive.Push(element);
+            element.SetActive(false);
+            element.SendMessage("OnDespawn");
+            element.transform.parent = transform;
+        }
+
+        // Awake is called when the script element is being loaded
+        public void Awake()
+        {
+        }
+    }
+
+    [Serializable]
+    public struct PredefinedObjectPool
+    {
+        [SerializeField] [Tooltip("The base prefab of this pool")] public GameObject Prefab;
+        [SerializeField] [Tooltip("The minimum number of objects to keep")] public uint Min;
+        [SerializeField] [Tooltip("The maximum number of objects to keep")] public uint Max;
+        [SerializeField] [Range(0.0f, 1.0f)] [Tooltip("The target usage ratio")] public float UsageRatio;
+    }
+
+    public interface IPooleableGameObject
+    {
+        void OnSpawn();
+        void OnDespawn();
+    }
+}
---
+++ Assets/PoolManager/Scripts/PoolManager.cs
@@ -0,0 +1,83 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace PoolingSystem
+{
+    public class PoolManager : MonoBehaviour
+    {
+        private static PoolManager _instance;
+
+        public static PoolManager Instance
+        {
+            get
+            {
+                if (!_instance)
+                    _instance = new GameObject("PoolManager").AddComponent<PoolManager>();
+                return _instance;
+            }
+        }
+
+        [SerializeField] [Tooltip("Predefined pools")] private PredefinedObjectPool[] _predefinedPools;
+        private readonly Dictionary<GameObject, ObjectPool> _pools = new Dictionary<GameObject, ObjectPool>();
+
+        public ObjectPool this[GameObject key]
+        {
+            get { return _pools[key]; }
+            protected set { _pools[key] = value; }
+        }
+
+        // Awake is called when the script instance is being loaded
+        public void Awake()
+        {
+            if (!_instance)
+                _instance = this;
+            else if (this != _instance)
+            {
+                Destroy(this);
+                return;
+            }
+
+            //Load all the pools defined in the inspector.
+            if (_predefinedPools != null)
+                foreach (var predefinedPool in _predefinedPools)
+                {
+                    var pool = new GameObject(predefinedPool.Prefab.name + "Pool").AddComponent<ObjectPool>();
+                    pool.transform.parent = transform;
+                    pool.FromPredefined(predefinedPool);
+                    this[pool.Prefab] = pool;
+                }
+        }
+
+        // This function is called when the MonoBehaviour will be destroyed
+        public void OnDestroy()
+        {
+            if (_instance == this)
+                _instance = null;
+        }
+    }
+
+    class InstanceIDComparer : IEqualityComparer<Object>
+    {
+        public bool Equals(Object x, Object y)
+        {
+            return x.GetInstanceID() == y.GetInstanceID();
+        }
+
+        public int GetHashCode(Object obj)
+        {
+            return obj.GetInstanceID();
+        }
+    }
+
+    public interface IObjectPool
+    {
+        GameObject Spawn();
+        GameObject Spawn(Transform transform);
+        GameObject Spawn(Transform transform, Transform parent);
+        GameObject Spawn(Vector3 position, Quaternion rotation);
+        GameObject Spawn(Vector3 position, Quaternion rotation, Transform parent);
+        void Despawn();
+        void Despawn(GameObject element);
+    }
+}
