9778d0a964431968b54065e7feb31decb7a46fd1
Camiloasc1 <camiloasc1@gmail.com>
2016-07-14 17:31:33 -0500

    IObjectPool moved to own file.

--- Assets/PoolManager/Scripts/PoolManager.cs
+++ Assets/PoolManager/Scripts/PoolManager.cs
@@ -89,6 +89,32 @@ namespace PoolingSystem
             _initialized = true;
         }
 
+        /// <summary>
+        /// Runs the selected Factory and GarbageCollector.
+        /// </summary>
+        /// <returns></returns>
+        private IEnumerator Worker()
+        {
+            poolingSetting.Factory.GetInstance().Setup(poolingSetting.FactoryParameters);
+            poolingSetting.GarbageCollector.GetInstance().Setup(poolingSetting.GarbageCollectorParameters);
+            while (true)
+            {
+                poolingSetting.Factory.GetInstance().Run();
+                poolingSetting.GarbageCollector.GetInstance().Run();
+                yield return null;
+            }
+        }
+
+        public IEnumerator<IObjectPool> GetEnumerator()
+        {
+            return _pools.Values.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
         // Awake is called when the script instance is being loaded
         public void Awake()
         {
@@ -118,28 +144,6 @@ namespace PoolingSystem
             _instance = null;
             StopCoroutine(_worker);
         }
-
-        private IEnumerator Worker()
-        {
-            poolingSetting.Factory.GetInstance().Setup(poolingSetting.FactoryParameters);
-            poolingSetting.GarbageCollector.GetInstance().Setup(poolingSetting.GarbageCollectorParameters);
-            while (true)
-            {
-                poolingSetting.Factory.GetInstance().Run();
-                //poolingSetting.GarbageCollector.GetInstance().Run();
-                yield return null;
-            }
-        }
-
-        public IEnumerator<IObjectPool> GetEnumerator()
-        {
-            return _pools.Values.GetEnumerator();
-        }
-
-        IEnumerator IEnumerable.GetEnumerator()
-        {
-            return GetEnumerator();
-        }
     }
 
     [Serializable]
@@ -166,118 +170,5 @@ namespace PoolingSystem
         [SerializeField] [Tooltip("The minimum number of instances to keep.")] public uint Min;
         [SerializeField] [Tooltip("The maximum number of instances to keep.\n(0 = No limit.).")] public uint Max;
         [SerializeField] [Range(0.1f, 1.0f)] [Tooltip("The target usage ratio.")] public float UsageRatio;
-    }
-
-    public interface IObjectPool
-    {
-        /// <summary>
-        /// The prefab that defines the pool.
-        /// </summary>
-        GameObject Prefab { get; }
-
-        /// <summary>
-        /// The minimum number of instances to keep.
-        /// </summary>
-        uint Min { get; set; }
-
-        /// <summary>
-        /// The maximum number of instances to keep.
-        /// </summary>
-        uint Max { get; set; }
-
-        /// <summary>
-        /// The target usage ratio.
-        /// </summary>
-        float Ratio { get; set; }
-
-        /// <summary>
-        /// The ammount of elements currently allocated.
-        /// </summary>
-        int Count { get; }
-
-        /// <summary>
-        /// The ammount of elements currently active.
-        /// </summary>
-        int ActiveCount { get; }
-
-        /// <summary>
-        /// The ammount of elements currently inactive.
-        /// </summary>
-        int InactiveCount { get; }
-
-        /// <summary>
-        /// The current usage ratio.
-        /// </summary>
-        float CurrentRatio { get; }
-
-        /// <summary>
-        /// The ratio after creating one instance.
-        /// </summary>
-        float NextRatio { get; }
-
-        /// <summary>
-        /// The ratio after destroying one instance.
-        /// </summary>
-        float PreviousRatio { get; }
-
-        /// <summary>
-        /// Retrieves or instantiates a new one if the pool is empty and the limit is not exceeded.
-        /// Internally called by the other Spawn() methods.
-        /// </summary>
-        /// <returns>A prefab instance ready to use.</returns>
-        GameObject Spawn();
-
-        /// <summary>
-        /// Retrieves or instantiates a new one if the pool is empty and the limit is not exceeded.
-        /// </summary>
-        /// <param name="transform">The transform with position and rotation to set.</param>
-        /// <returns>A prefab instance ready to use</returns>
-        GameObject Spawn(Transform transform);
-
-        /// <summary>
-        /// Retrieves or instantiates a new one if the pool is empty and the limit is not exceeded.
-        /// </summary>
-        /// <param name="transform">The transform with position and rotation to set.</param>
-        /// <param name="parent">The parent transform in the scene to set.</param>
-        /// <returns>A prefab instance ready to use</returns>
-        GameObject Spawn(Transform transform, Transform parent);
-
-        /// <summary>
-        /// Retrieves or instantiates a new one if the pool is empty and the limit is not exceeded.
-        /// </summary>
-        /// <param name="position">The position to set.</param>
-        /// <param name="rotation">The rotation to set.</param>
-        /// <returns>A prefab instance ready to use</returns>
-        GameObject Spawn(Vector3 position, Quaternion rotation);
-
-        /// <summary>
-        /// Retrieves or instantiates a new one if the pool is empty and the limit is not exceeded.
-        /// </summary>
-        /// <param name="position">The position to set.</param>
-        /// <param name="rotation">The rotation to set.</param>
-        /// <param name="parent">The parent transform in the scene to set.</param>
-        /// <returns>A prefab instance ready to use</returns>
-        GameObject Spawn(Vector3 position, Quaternion rotation, Transform parent);
-
-        /// <summary>
-        /// Forcedly despawn an active instance.
-        /// </summary>
-        void Despawn();
-
-        /// <summary>
-        /// Despawn the selected instance.
-        /// </summary>
-        /// <param name="instance">The instance to despawn</param>
-        void Despawn(GameObject instance);
-
-        /// <summary>
-        /// Forcedly create a new instance.
-        /// </summary>
-        bool Instantiate();
-
-        /// <summary>
-        /// Forcedly destroy an inactive instance.
-        /// </summary>
-        bool Destroy();
     }
 }
